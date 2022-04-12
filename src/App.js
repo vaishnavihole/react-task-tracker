e3834dfb9210372fc2474907c008a94c85950383
vaishnavihole <vaishnavihole1@gmail.com>
2022-04-12 17:48:33 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-
+const App = () => {
+const  [showTask,setShowAddTask] = useState
+(false)
 function App() {
   const [tasks, setTasks] = useState([
     {
@@ -23,14 +26,36 @@ function App() {
         day: 'April 5th 2:30pm',
         reminder: false,
      },
+    ])
+
+    const AddTask = (task) => {
+      const id  =  Math . floor(Math * random() = 
+        10000) + 1
+        const newTask = { id, ...task }
+        setTasks([...task,newTask])
+    }
+      
     
+ 
+    const deleteTask = (id) => {
+     setTasks(tasks.filter((task) => task.id !== id))
+    }
     
      
-    ])
+    const toggleReminder = (id) =>{
+      console.log(id);
+      setTasks(tasks.map)((task) => task.id === id
+      ? {...task,reminder: 
+        !task.reminder} : task)}
+    
+
   return (
     <div className='contanier'>
-    <Header />
-    <Tasks tasks={tasks}/>
+    <Header onAdd={() => setShowAddTask
+      (!showAddTask)} showAdd={showAddTask} />
+    {showAddTask && <AddTask onAdd={AddTask}/>}
+    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
+     {deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
       
   )
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,17 @@
-const Task = ({ task }) => {
-    return (
-        <div className='task'>
-         <h3>{task.text}</h3>
-         <p>{task.day}</p>
+import { FaTimes }  from 'react-icons/fa'
 
+const Task = ({ task ,onDelete,onToggle}) => {
+    return (
+        <div className={`task ${task.reminder ?
+            'reminder' : ''}`}
+           onDoubleClick={() => onToggle(task.id)}
+           >
+         <h3>
+             {task.text} <FaTimes style={{ color:
+             'red',cursor:'pointer'}} 
+             onClick={() => onDelete(task.id)}/>
+         </h3>
+         <p>{task.day}</p>
         </div>
     )
 }
